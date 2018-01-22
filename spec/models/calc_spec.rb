c8e53d8521065bf220386deb2f7788f4892e8945
slim1979 <chinezan@yandex.ru>
2018-01-23 02:57:53 +0300

    improved

--- spec/models/calc_spec.rb
+++ spec/models/calc_spec.rb
@@ -1,9 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Calc, type: :model do
-  it { should validate_presence_of :delivery_from }
-  it { should validate_presence_of :delivery_to }
+  it { should validate_presence_of :delivery_from_id }
+  it { should validate_presence_of :delivery_to_id }
   it { should validate_presence_of :cargo_width }
-  it { should validate_presence_of :cargo_heigth }
+  it { should validate_presence_of :cargo_height }
   it { should validate_presence_of :cargo_depth }
+  it { should belong_to :delivery_from }
+  it { should belong_to :delivery_to }
 end
