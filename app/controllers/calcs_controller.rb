3664fdf4acd17d018f5e8b5d72bbf075641b1eaf
slim1979 <chinezan@yandex.ru>
2018-01-23 04:48:22 +0300

    render json instead of responders

--- app/controllers/calcs_controller.rb
+++ app/controllers/calcs_controller.rb
@@ -1,2 +1,19 @@
 class CalcsController < ApplicationController
+
+  respond_to :html, only: :index
+
+  def index
+    respond_with(@calcs = Calc.all)
+  end
+
+  def create
+    @calc = Calc.create(calc_params)
+    render json: { calc: @calc, from: @calc.delivery_from.title, _to: @calc.delivery_to.title }
+  end
+
+  private
+
+  def calc_params
+    params.require(:calc).permit(:delivery_from_id, :delivery_to_id, :cargo_width, :cargo_height, :cargo_depth)
+  end
 end
