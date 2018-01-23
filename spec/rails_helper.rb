32d37d930838f2916ef7d97c87ec73519f5f17c1
slim1979 <chinezan@yandex.ru>
2018-01-23 03:19:21 +0300

    capybara server & driver, fb syntax

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -29,7 +29,10 @@ ActiveRecord::Migration.maintain_test_schema!
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.include FactoryBot::Syntax::Methods
   Capybara.javascript_driver = :webkit
+  Capybara.current_driver = Capybara.javascript_driver
+  Capybara.server = :puma
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
